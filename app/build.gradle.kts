1b92ba88b6014fc6c490a7f6fb6252ae9e83f972
Luke <51413507+lukeponga-dev@users.noreply.github.com>
2026-09-18 02:32:05 +1200

    build: bump app version to 1.1.2 beta
    Update version code to 5 and version name to 1.1.2 beta for release.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -17,8 +17,8 @@ android {
     applicationId = "dev.lukeponga.pricesnap"
     minSdk = 26
     targetSdk = 36
-    versionCode = 4
-    versionName = "3.1"
+    versionCode = 5
+    versionName = "1.1.2 beta"
 
     testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
   }
