c72dd8a679d9acc1913c55d662bc52a9a2765348
Luke <51413507+lukeponga-dev@users.noreply.github.com>
2026-09-18 01:49:22 +1200

    build: bump version to 3.1 (code 4)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -17,8 +17,8 @@ android {
     applicationId = "dev.lukeponga.pricesnap"
     minSdk = 26
     targetSdk = 36
-    versionCode = 3
-    versionName = "3.0"
+    versionCode = 4
+    versionName = "3.1"
 
     testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
   }
